afd4ed4255faed27f35a4b85811c72653d964597
bb0b <konlawatit@gmail.com>
2020-10-25 15:12:27 +0700

    update android11

--- files/js/android.11/android11.js
+++ files/js/android.11/android11.js
@@ -2,8 +2,11 @@ var controller = new ScrollMagic.Controller();
 
 // build tween
 var tween = TweenMax.to("#animate", 0.5, {
-    width: 1000,
+    width:1000,
     height:1000,
+    top:0,
+    transformOrigin: "center center", 
+    
     ease: Linear.easeNone
 });
 
@@ -11,27 +14,27 @@ var tween = TweenMax.to("#animate", 0.5, {
 var scene = new ScrollMagic.Scene({
         triggerElement: "#multiDirect",
         duration: 700,
-        offset: 250
+     
     })
     .setTween(tween)
     .setPin("#animate")
     .addTo(controller);
 
-// init controller horizontal
-var controller_h = new ScrollMagic.Controller({
-    vertical: false
-});
+// // init controller horizontal
+// var controller_h = new ScrollMagic.Controller({
+//     vertical: false
+// });
 
-// build tween horizontal
-var tween_h = TweenMax.to("#animate", 0.5, {
-    rotation: 360,
-    ease: Linear.easeNone
-});
+// // build tween horizontal
+// var tween_h = TweenMax.to("#animate", 0.5, {
+//     rotation: 360,
+//     ease: Linear.easeNone
+// });
 
-// build scene
-var scene_h = new ScrollMagic.Scene({
-        duration: 700
-    })
-    .setTween(tween_h)
-    .setPin("#animate")
-    .addTo(controller_h);
+// // build scene
+// var scene_h = new ScrollMagic.Scene({
+//         duration: 700
+//     })
+//     .setTween(tween_h)
+//     .setPin("#animate")
+//     .addTo(controller_h);
