9b759d1f2c1113daddbb4e63658b69f76bc75eb4
kuns019 <56548175+kuns019@users.noreply.github.com>
2020-11-01 18:28:19 +0700

    full animation

--- script.js
+++ script.js
@@ -5,3 +5,62 @@ gsap.to(".img3", {x: -40, y: 170, duration: 1.5});
 gsap.to(".img4", {x: 50, y: -90, duration: 1.7});
 gsap.to(".c1", {scale: 4.2, duration: 1.8});
 gsap.to(".c2", {x: -1340, y: -680, duration: 1.5});
+  
+gsap.utils.toArray(".d1").forEach(dhov => {
+    const c = document.querySelector(".c3");
+    let hover = gsap.to(c, {
+        scale: 5, 
+        duration: 0.5,
+        y: -600, 
+        paused: true, 
+        ease: "power1.inOut"
+    });
+    dhov.addEventListener("mouseenter", () => hover.play());
+    dhov.addEventListener("mouseleave", () => hover.reverse());
+});
+gsap.utils.toArray(".d2").forEach(dhov => {
+    const c = document.querySelector(".c4");
+    let hover = gsap.to(c, {
+        scale: 5, 
+        duration: 0.5,
+        y: -600, 
+        paused: true, 
+        ease: "power1.inOut"
+    });
+    dhov.addEventListener("mouseenter", () => hover.play());
+    dhov.addEventListener("mouseleave", () => hover.reverse());
+});
+gsap.utils.toArray(".d3").forEach(dhov => {
+    const c = document.querySelector(".c5");
+    let hover = gsap.to(c, {
+        scale: 5, 
+        duration: 0.5,
+        y: -600, 
+        paused: true, 
+        ease: "power1.inOut"
+    });
+    dhov.addEventListener("mouseenter", () => hover.play());
+    dhov.addEventListener("mouseleave", () => hover.reverse());
+});
+
+TweenLite.defaultEase = Linear.easeNone;
+var titles = document.querySelectorAll(".pmove");
+var controller = new ScrollMagic.Controller();
+var tl = new TimelineMax();
+
+// create timeline
+// this could also be created in a loop
+tl.to(".pmain", 1, {xPercent: 2}, "label1");
+tl.from(titles[1], 0.5, {opacity:0}, "label1+=0.5");
+tl.to(".pmain", 1, {xPercent: -25}, "label2");
+tl.from(titles[2], 0.5, {opacity:0}, "label2+=0.5");
+
+
+
+
+new ScrollMagic.Scene({
+  triggerHook: "onLeave",
+  duration: "400%"
+})
+  .setTween(tl)
+  .addTo(controller);
